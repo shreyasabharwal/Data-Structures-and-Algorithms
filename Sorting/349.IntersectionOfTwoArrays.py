f63b22d6bbdcb1ac8e02959f654286bb4c4f6dc0
Shreya Sabharwal <sabhas2@uw.edu>
2019-06-19 18:04:24 -0700

    leetcode easy - sort

---
+++ Sorting/349.IntersectionOfTwoArrays.py
@@ -0,0 +1,35 @@
+'''349. Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.'''
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        if len(nums1) < len(nums2):
+            self.intersection(nums2, nums1)
+
+        # create set
+        s_counts = set(nums2)
+        ans = set()
+        for num in nums1:
+            if num in s_counts:
+                ans.add(num)
+        return ans
---
+++ Sorting/976.LargestPerimeterTriangle.py
@@ -0,0 +1,41 @@
+'''976. Largest Perimeter Triangle
+
+Given an array A of positive lengths, return the largest perimeter of a triangle with non-zero area, formed from 3 of these lengths.
+If it is impossible to form any triangle of non-zero area, return 0.
+
+Example 1:
+Input: [2,1,2]
+Output: 5
+
+Example 2:
+Input: [1,2,1]
+Output: 0
+
+Example 3:
+Input: [3,2,3,4]
+Output: 10
+
+Example 4:
+Input: [3,6,2,3]
+Output: 8
+ 
+
+Note:
+3 <= A.length <= 10000
+1 <= A[i] <= 10^6'''
+
+
+class Solution(object):
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+
+        maxArea = 0
+        A = sorted(A, reverse=True)
+        for i in range(len(A)-2):
+            if A[i] < A[i+1]+A[i+2]:
+                maxArea = max(maxArea, sum(A[i:i+3]))
+
+        return maxArea
